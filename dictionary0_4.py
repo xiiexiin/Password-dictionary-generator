70adba2ab5a239bd50e5188367178c4b16618cb4
xiiexiin <58377323+xiiexiin@users.noreply.github.com>
2020-03-06 17:48:28 +0800

    Delete dictionary0_4.py

--- dictionary0_4.py
+++
@@ -1,9 +0,0 @@
-import itertools as its
-words = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for k in range(1,5):
-    r =its.product(words,repeat=k)
-    dic = open("/home/lwc/Documents/学习/网络攻防/编程实验/第一天/dictionary.txt","a")
-    for i in r:
-        dic.write("".join(i))
-        dic.write("".join("\n"))
-    dic.close()
